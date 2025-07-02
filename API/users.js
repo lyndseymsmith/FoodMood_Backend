a1aff74737aa7c4a0771529713da44e2eb64d019
Danielle <dani.camp1125@gmail.com>
2025-07-01 21:58:50 -0400

    starting api for /register--Dani

--- API/users.js
+++ API/users.js
@@ -1,5 +1,21 @@
-import express from "express";
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router();
-export default router;
+const JWT_SECRET = 'FoodMood2025'
 
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    try{
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await registerUser(username, hashed);
+        res.status(201).json({message: 'User registered', user})
+    }catch(error){
+        
+    }
+})
+
+export default router;
