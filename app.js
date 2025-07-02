614dc7d63b7d4376d3093ecfdbc8b6a7136888d6
Danielle <dani.camp1125@gmail.com>
2025-07-01 21:16:58 -0400

    Added framework for api users.js and added res.status(500) for app.js --Dani

---
+++ app.js
@@ -0,0 +1,17 @@
+import usersRouter from './API/users.js';
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.use('users', usersRouter);
+
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send("Sorry! Something went wrong")
+});
+
+export default app; 
