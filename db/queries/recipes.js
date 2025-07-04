03af586124474047c075fb51279f57b7948b0716
Trice3zz <dariustrice.98@gmail.com>
2025-07-03 21:04:00 -0400

    query update

---
+++ db/queries/recipes.js
@@ -0,0 +1,30 @@
+
+
+const pool = require('./db'); 
+
+
+const getAllRecipes = async () => {
+  const result = await pool.query('SELECT * FROM recipes');
+  return result.rows;
+};
+
+
+const getRecipesByMood = async (mood) => {
+  const result = await pool.query('SELECT * FROM recipes WHERE mood = $1', [mood]);
+  return result.rows;
+};
+
+
+const getRecipeById = async (id) => {
+  const result = await pool.query('SELECT * FROM recipes WHERE id = $1', [id]);
+  return result.rows[0];
+};
+
+
+
+module.exports = {
+  getAllRecipes,
+  getRecipesByMood,
+  getRecipeById,
+  
+};
