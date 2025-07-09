af34b9764f7a6fc0c14fe6a3d2f4e5f9fbc52a14
Danielle <dani.camp1125@gmail.com>
2025-07-09 19:44:11 -0400

    cleaned up server.js to display PORT only with app.use authRouter--Dani Also moved unnecessary information that was in server.js to necessary file recipes.js

--- Server.js
+++ Server.js
@@ -1,42 +1,9 @@
 
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const PORT = 3000;
+import app from './app.js';
+import client from './db/client.js';
+import authRouter from '../Middleware/middleware.js'
+app.use('/users', authRouter)
 
-const recipes = require('./recipes');
-
-app.use(cors());
-app.use(express.json());
-
-
-app.get('/recipes', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM recipes');
-    res.json(result.rows);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-
-
-app.get('/recipes/:mood', async (req, res) => {
-  const { mood } = req.params;
-  try {
-    const result = await pool.query(
-      'SELECT * FROM recipes WHERE mood = $1',
-      [mood.toLowerCase()]
-    );
-    res.json(result.rows);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// import app from './app.js
-//import client from './db/client.js
-
-//cont PORT = process.env.PORT ?? 3000;
-//await client.connect()
+const PORT = process.env.PORT ?? 3000;
+await client.connect()
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
