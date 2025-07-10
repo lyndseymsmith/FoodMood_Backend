5297f0742122e51704304806e42d6ea9da06fae2
Lyndsey <lyndseymsmith44@gmail.com>
2025-07-09 20:44:40 -0400

    playlist and seed edits

--- db/queries/recipes.js
+++ db/queries/recipes.js
@@ -1,30 +1,28 @@
-
-
 // const pool = require('./db'); 
+import client from '../client.js';
 
-
-const getAllRecipes = async () => {
+export async function getAllRecipes() {
   const result = await client.query('SELECT * FROM recipes');
   return result.rows;
 };
 
 
-const getRecipesByMood = async (mood) => {
+export async function getRecipesByMood(mood) {
   const result = await client.query('SELECT * FROM recipes WHERE mood = $1', [mood]);
   return result.rows;
 };
 
 
-const getRecipeById = async (id) => {
+export async function getRecipeById(id) {
   const result = await client.query('SELECT * FROM recipes WHERE id = $1', [id]);
   return result.rows[0];
 };
 
 
 
-module.exports = {
-  getAllRecipes,
-  getRecipesByMood,
-  getRecipeById,
+// module.exports = {
+//   getAllRecipes,
+//   getRecipesByMood,
+//   getRecipeById,
   
-};
+// };
