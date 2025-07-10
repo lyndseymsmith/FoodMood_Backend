510e4b1bb18b18d46fca51a5fd1f1f7cca8d57ce
Lyndsey <lyndseymsmith44@gmail.com>
2025-07-09 20:27:53 -0400

    Edits to seed.js and mood.js for playlists

--- db/queries/mood.js
+++ db/queries/mood.js
@@ -1,6 +1,7 @@
 import client from '../client.js'
 
-export async function getMoods(){
-    const result = await client.query('SELECT * FROM moods;')
-    return result.rows;
+export async function getMoods(name, description){
+    const result = await client.query(`INSERT INTO moods (name, description) VALUES ($1, $2) RETURNING *;`, [name, description]
+    );
+    return result.rows[0];
 };
