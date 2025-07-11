18117447ef99e88472852ae77554dbf392144c55
Danielle <dani.camp1125@gmail.com>
2025-07-10 20:28:22 -0400

    removed /users and left only authenticateToken

--- Server.js
+++ Server.js
@@ -2,7 +2,10 @@
 import app from './app.js';
 import client from './db/client.js';
 import {authenticateToken} from './Middleware/middleware.js';
-app.use('/users', authenticateToken)
+import dotenv from 'dotenv';
+dotenv.config()
+
+app.use(authenticateToken)
 
 const PORT = process.env.PORT ?? 3000;
 await client.connect()
