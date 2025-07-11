c62a3084c2afa1a6b2346f6aa8aff8ec51d3fac6
Danielle <dani.camp1125@gmail.com>
2025-07-10 20:59:06 -0400

    changed createMoods to getMoods to fetch the moods

--- API/mood.js
+++ API/mood.js
@@ -2,9 +2,9 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-import { createMoods } from "../db/queries/mood.js";
+import { getMoods } from "../db/queries/mood.js";
 
 router.route("/").get(async (req, res) => {
-    const moods = await createMoods();
+    const moods = await getMoods();
     res.send(moods);
 });
