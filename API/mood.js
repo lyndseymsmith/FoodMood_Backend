d1c444cd3d5cd3472e8307b07949190913cefe26
Danielle <dani.camp1125@gmail.com>
2025-07-10 21:42:01 -0400

    added some response statuses to assist with the handle error

--- API/mood.js
+++ API/mood.js
@@ -1,10 +1,17 @@
 import express from "express";
 const router = express.Router();
-export default router;
+
 
 import { getMoods } from "../db/queries/mood.js";
 
 router.route("/").get(async (req, res) => {
-    const moods = await getMoods();
-    res.send(moods);
+    try{
+     const moods = await getMoods();
+    res.send(moods);   
+    }catch(error){
+        console.error("Error fetching moods: error")
+        res.status(500).json({error: "Failed to fetch moods"})
+    }
+    
 });
+export default router;
