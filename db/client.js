cdab01327115941768463da196a919767e4337c1
Danielle <dani.camp1125@gmail.com>
2025-07-13 14:49:44 -0400

    small spacing to see if it was affecting any errors

--- db/client.js
+++ db/client.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config()
 
 console.log(process.env.DATABASE_URL)
-const client = new Client({connectionString: process.env.DATABASE_URL})
+const client = new Client({ connectionString: process.env.DATABASE_URL })
+
 
 export default client;
