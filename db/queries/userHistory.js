43c0299123e2999f279e4f5a5142838664fc4a90
Danielle <dani.camp1125@gmail.com>
2025-07-13 17:46:32 -0400

    created an insert query iwth user_id and mood_id

--- db/queries/userHistory.js
+++ db/queries/userHistory.js
@@ -1,10 +1,20 @@
 import client from "../client.js";
 
+
+
 export async function getSavedByUserId(user_id) {
   const sql = await client.query(
-    `SELECT saved.*, recipes.title, mood.emotion FROM saved JOIN recipes ON saved.recipes_id = recipes.id JOIN mood ON saved.mood_id = mood.id WHERE saved.user_id = $1;`,
+    `SELECT user_history.*, recipes.title, mood.emotion FROM user_history JOIN recipes ON user_history.recipes_id = recipes.id JOIN mood ON user_history.mood_id = mood.id WHERE user_history.user_id = $1;`,
     [user_id]
   );
 
   return sql.rows;
+}
+
+export async function saveUserMood(user_id, mood_id){
+  const sql = await client.query(
+    `INSERT INTO user_history (user_id, mood_id) VALUES ($1, $2);`,
+    [user_id, mood_id]
+  );
+  return sql.rows[0];
 }
