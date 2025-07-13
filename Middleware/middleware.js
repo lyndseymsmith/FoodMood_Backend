5cf01130d55435107337909f027d262b05ab5c49
Danielle <dani.camp1125@gmail.com>
2025-07-13 17:26:55 -0400

    moved code in middleware to possibly more correct locations

--- Middleware/middleware.js
+++ Middleware/middleware.js
@@ -2,14 +2,14 @@ import jwt from 'jsonwebtoken';
 
 export function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
+    
     const token = authHeader.split(' ')[1];
-    const decodedJWT = jwt.verify(token, process.env.JWT_SECRET);
-
     if (!authHeader) {
         return res.status(401).json({ error: 'Token required' });
     }
 
     try {
+        const decodedJWT = jwt.verify(token, process.env.JWT_SECRET || 'FoodMood2025');
         req.user = decodedJWT;
         next();
     } catch (error) {
