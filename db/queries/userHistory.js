828e6eadc2e19d9989759e071ebb69d00618cc86
Danielle <dani.camp1125@gmail.com>
2025-07-13 19:55:37 -0400

    corrected syntax and misspellings

--- db/queries/userHistory.js
+++ db/queries/userHistory.js
@@ -4,7 +4,7 @@ import client from "../client.js";
 
 export async function getSavedByUserId(user_id) {
   const sql = await client.query(
-    `SELECT user_history.*, recipes.title, mood.emotion FROM user_history JOIN recipes ON user_history.recipes_id = recipes.id JOIN mood ON user_history.mood_id = mood.id WHERE user_history.user_id = $1;`,
+    `SELECT user_history.*, moods.emotion FROM user_history JOIN recipes ON user_history.recipes_id = recipes.id JOIN moods ON user_history.mood_id = moods.id WHERE user_history.user_id = $1;`,
     [user_id]
   );
 
@@ -12,9 +12,9 @@ export async function getSavedByUserId(user_id) {
 }
 
 export async function saveUserMood(user_id, mood_id){
-  const sql = await client.query(
-    `INSERT INTO user_history (user_id, mood_id) VALUES ($1, $2);`,
-    [user_id, mood_id]
-  );
+    const sql = await client.query(
+    `INSERT INTO user_history (user_id, mood_id, action_type) VALUES ($1, $2, $3) RETURNING *;`,
+    [user_id, mood_id, 'tracked']
+    );
   return sql.rows[0];
 }
