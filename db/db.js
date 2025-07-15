3f0bae6803f534515b404816d0f6b352a99098dd
Danielle <dani.camp1125@gmail.com>
2025-07-14 20:56:26 -0400

    removed commented out code

--- db/db.js
+++ db/db.js
@@ -1,11 +1,1 @@
-// const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'foodmood',
-//   password: 'Freeman0228$', // 
-//   port: 5432,
-// });
-
-// module.exports = pool;
