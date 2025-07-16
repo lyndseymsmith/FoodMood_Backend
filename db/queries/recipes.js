5f01ba18645fd524ca11d3f9a8cc5df4c905e4c5
Trice3zz <dariustrice.98@gmail.com>
2025-07-15 21:38:56 -0400

    imgurl

--- db/queries/recipes.js
+++ db/queries/recipes.js
@@ -1,14 +1,14 @@
+import client from "../client.js";
 
-import client from '../client.js';
 
-export async function createRecipes(title, mood_id, description, ingredients, instructions) {
+export async function createRecipes(title, mood_id, description, ingredients, instructions, image_url) {
   const result = await client.query(
     `
-    INSERT INTO recipes (title, mood_id, description, ingredients, instructions)
-    VALUES ($1, $2, $3, $4, $5)
+    INSERT INTO recipes (title, mood_id, description, ingredients, instructions, image_url)
+    VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING *;
     `,
-    [title, mood_id, description, ingredients, instructions]
+    [title, mood_id, description, ingredients, instructions, image_url]
   );
 
   return result.rows[0];
@@ -16,20 +16,25 @@ export async function createRecipes(title, mood_id, description, ingredients, in
 
 
 export async function getAllRecipes() {
-  const result = await client.query('SELECT * FROM recipes');
+  const result = await client.query("SELECT * FROM recipes");
   return result.rows;
-};
+}
 
 
-export async function getRecipesByMood(emotion) {
-  const result = await client.query('SELECT * FROM recipes WHERE emotion = $1', [emotion]);
+export async function getRecipesByMood(mood_id) {
+  const result = await client.query("SELECT * FROM recipes WHERE mood_id = $1", [mood_id]);
   return result.rows;
-};
+}
 
 
 export async function getRecipeById(id) {
-  const result = await client.query('SELECT * FROM recipes WHERE id = $1', [id]);
+  const result = await client.query(
+    `
+    SELECT id, title, mood_id, description, ingredients, instructions, image_url
+    FROM recipes
+    WHERE id = $1
+    `,
+    [id]
+  );
   return result.rows[0];
-};
-
-
+}
